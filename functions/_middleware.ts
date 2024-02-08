e3d640abe1df033b4e7745a5ba4b3ff31114da82
Ludwig Stecher <ludwig.stecher@gmx.de>
2024-02-08 20:39:24 +0100

    fix email sender

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -9,7 +9,7 @@ export const onRequest: PagesFunction = mailChannelsPlugin({
 	from: {
 		name: 'Queeres Zentrum Kassel',
 		// The domain of your `from` address must be the same as the domain you set up MailChannels Domain Lockdown for (detailed below)
-		email: 'info@qzk.pages.dev',
+		email: 'info@aloso.foo',
 	},
 	subject: ({ formData }: { formData: FormData }) => `[QZK Formular] ${formData.get('subject')}`,
 	respondWith: () => {
