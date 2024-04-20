f1c7e0f0ea65688b2e49c1e900ca01a28437cbab
Ludwig Stecher <ludwig.stecher@gmx.de>
2024-04-20 18:45:53 +0200

    fix mobile nav

--- src/lib/components/nav/MobileNav.svelte
+++ src/lib/components/nav/MobileNav.svelte
@@ -55,8 +55,8 @@
 			display: flex;
 			flex-direction: column;
 			box-sizing: border-box;
-			height: calc(90vh - 12rem);
-			margin-top: calc(12rem - 90vh);
+			height: calc(92vh - 12rem);
+			margin-top: calc(12rem - 92vh);
 			background-color: vars.$COLOR_T3;
 			overflow: hidden;
 			transition: 0.4s;
@@ -78,7 +78,8 @@
 		}
 
 		.mobile-nav-inner {
-			padding: 1.8rem;
+			padding: 1.5rem;
+			padding-bottom: 3rem;
 			overflow: auto;
 
 			.nav-group {
@@ -91,7 +92,7 @@
 
 				&:global(.extended) {
 					box-shadow: 0 0 0 3px #0002;
-					padding-bottom: 1rem;
+					padding-bottom: 0.7rem;
 
 					.nav-extender {
 						transform: rotate(0deg);
@@ -100,7 +101,7 @@
 					.nav-child {
 						display: block;
 						line-height: 1;
-						padding: 1rem 2rem;
+						padding: 0.83rem 1.4rem;
 						overflow: hidden;
 					}
 				}
@@ -112,7 +113,7 @@
 
 			.nav-child {
 				line-height: 0;
-				padding: 0 2rem;
+				padding: 0 1.4rem;
 				overflow: hidden;
 				transition:
 					line-height 0.2s,
@@ -123,9 +124,10 @@
 				flex-grow: 0;
 				border: none;
 				border-radius: 100px;
-				box-shadow: inset 0 0 0 0.66rem vars.$COLOR_T3;
+				box-shadow: inset 0 0 0 0.33rem vars.$COLOR_T3;
 				background-color: transparent;
-				width: calc(2.8rem + 25px);
+				width: calc(2rem + 19px);
+				margin: 3px;
 				padding: 0;
 				background-image: url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8" width="8" height="8"><path style="fill: none; stroke: white; stroke-linejoin: round; stroke-linecap: round" d="M1 3 L 4 6 L 7 3" /></svg>');
 				background-repeat: no-repeat;
@@ -150,17 +152,17 @@
 				display: block;
 				flex-grow: 1;
 				color: white;
-				padding: 1.4rem;
+				padding: 1rem 1.4rem;
 				text-decoration: none;
 				font-weight: 500;
-				font-size: 1.4rem;
+				font-size: 1.25rem;
 				line-height: 25px;
 				border-radius: 25px;
 
 				&.nav-child {
-					margin: 0 1rem;
+					margin: 0 0.7rem;
 					border-radius: 20px;
-					font-size: 1.3rem;
+					font-size: 1.1rem;
 				}
 
 				&:hover,
