b5939c97239209620cf3c58557d3339b577c0156
Ludwig Stecher <ludwig.stecher@gmx.de>
2024-05-06 22:35:06 +0200

    fix missing newsletter pages

--- src/routes/[...slug]/+page.server.ts
+++ src/routes/[...slug]/+page.server.ts
@@ -1,6 +1,15 @@
 import { loadStatic } from '$lib/contentful/loader'
 import type { LoadEvent } from '@sveltejs/kit'
 
+// This tells SvelteKit to pre-render the following pages, which aren't linked to from anywhere
+export function entries() {
+	return [
+		{ slug: 'newsletter/double-opt-in' },
+		{ slug: 'newsletter/angemeldet' },
+		{ slug: 'newsletter/abgemeldet' },
+	]
+}
+
 export async function load({ params }: LoadEvent<{ slug: string }>) {
 	return loadStatic(params.slug, 900)
 }
