6c2ad84be8c926738840a7de4bb2c5688d56d44b
Ludwig Stecher <ludwig.stecher@gmx.de>
2024-10-04 18:48:34 +0200

    fix error

--- src/lib/contentful/index.ts
+++ src/lib/contentful/index.ts
@@ -1,9 +1,9 @@
-import contentful from 'contentful'
+import { createClient } from 'contentful'
 export * from './types'
 
 const isDev = process.env.NODE_ENV === 'development'
 
-export const client = contentful.createClient({
+export const client = createClient({
 	space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
 	accessToken: isDev
 		? import.meta.env.VITE_CONTENTFUL_TOKEN_PREVIEW
