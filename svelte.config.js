a717f5dccca315fa1813ca959487fa48ee4ed9eb
Ludwig Stecher <ludwig.stecher@gmx.de>
2024-12-06 17:04:31 +0100

    feat: telegram build notifications

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import autoAdapter from '@sveltejs/adapter-auto'
 import staticAdapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { createClient } from './telegram/client.js'
+import dotenv from 'dotenv'
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -26,7 +28,60 @@ const config = {
 					precompress: false,
 					strict: true,
 				}),
+
+		prerender: {
+			async handleHttpError(event) {
+				await initClient()
+				await client.sendMessage(process.env.TELEGRAM_NOTIFICATION_RECIPIENT, {
+					message: `[QZK BOT] Beim Bauen der Anwendung ist ein Fehler aufgetreten: ${event.status}
+Pfad: ${event.path}
+Ursprungsseite: ${event.referrer}
+Art: ${event.referenceType}
+Fehlermeldung: ${event.message}`,
+				})
+				throw new Error(`Http error occurred: ${event.message}`)
+			},
+			async handleMissingId(input) {
+				await initClient()
+				await client.sendMessage(process.env.TELEGRAM_NOTIFICATION_RECIPIENT, {
+					message: `[QZK BOT] Beim Bauen der Anwendung ist ein Fehler aufgetreten: ID fehlt
+id: ${input.id}
+Pfad: ${input.path}
+Ursprungsseiten: ${input.referrers}
+Art: ${input.referenceType}
+Fehlermeldung: ${input.message}`,
+				})
+				throw new Error(`ID missing: ${input.message}`)
+			},
+			async handleEntryGeneratorMismatch(event) {
+				await initClient()
+				await client.sendMessage(process.env.TELEGRAM_NOTIFICATION_RECIPIENT, {
+					message: `[QZK BOT] Beim Bauen der Anwendung ist ein Fehler aufgetreten: Ein Eintrag stimmt nicht mit der Route überein, aus der er erzeugt wurde
+generatedFromId: ${event.generatedFromId}
+matchedId: ${event.matchedId}
+Eintrag: ${event.entry}
+Fehlermeldung: ${event.message}`,
+				})
+				throw new Error(`Http error occurred: ${event.message}`)
+			},
+		},
 	},
+}
+
+/** @type {import("telegram").TelegramClient} */
+let client
+
+async function initClient() {
+	if (!client) {
+		dotenv.config()
+		client = await createClient({
+			apiId: +process.env.TELEGRAM_API_ID,
+			apiHash: process.env.TELEGRAM_API_HASH,
+			stringSession: process.env.TELEGRAM_STRING_SESSION,
+			phoneNumber: process.env.TELEGRAM_PHONE_NUMBER,
+			password: process.env.TELEGRAM_PASSWORD,
+		})
+	}
 }
 
 export default config
