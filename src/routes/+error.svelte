d2841fb7f6bd7a6dd98396f5bf6d3987e51599de
Ludwig Stecher <ludwig.stecher@gmx.de>
2025-09-16 23:34:47 +0200

    chore: updat eslint

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { page } from '$app/stores'
+	import { page } from '$app/state'
 </script>
 
 <svelte:head>
@@ -7,16 +7,16 @@
 </svelte:head>
 
 <div class="inner">
-	{#if $page.status === 404}
+	{#if page.status === 404}
 		<h1>Seite nicht gefunden</h1>
 		<p>Entschuldigung, wir haben nicht gefunden, wonach Du suchst.</p>
 		<p>
 			<a href="/">Zur Startseite</a>
 		</p>
 	{:else}
-		<h1>Fehler {$page.status}</h1>
+		<h1>Fehler {page.status}</h1>
 		<p>
-			{$page.error!.message}
+			{page.error!.message}
 		</p>
 	{/if}
 </div>
